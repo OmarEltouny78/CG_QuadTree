22152691b6d49672504d5c102bc76f4bc447e5c4
OmarEltouny78 <omar.eltouny@uofcanada.edu.eg>
2021-03-14 20:55:31 +0200

    Assignment sub

---
+++ Text assignment/Assets/Texteffects.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Texteffects : MonoBehaviour
+{
+	public Text txt;
+	public string story;
+	public Color color;
+	public float value;
+	public float speed;
+	public float r;
+	public float g;
+	public float b;
+
+	void Awake()
+	{
+		txt = GetComponent<Text>();
+		story = txt.text;
+		txt.text = "";
+
+		
+		StartCoroutine("PlayText");
+
+	}
+	void Update()
+    {
+		color = txt.color;
+		ModifyOpacity();
+		ModifyColor();
+	}
+
+	void ModifyOpacity()
+    {
+		color.a=value;
+		txt.color = color;
+    }
+	void ModifyColor()
+    {
+		color.r = r;
+		color.g = g;
+		color.b = b;
+		txt.color = color;
+    }
+
+	IEnumerator PlayText()
+	{
+		foreach (char c in story)
+		{
+			txt.text += c;
+			yield return new WaitForSeconds(1/speed);
+		}
+	}
+}
